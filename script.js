cab0ec3372ceb01c79071d02829e4bad736a94b0
Alton <alton.ryld@gmail.com>
2023-09-16 09:25:13 +0200

    Mise en place du css et du chargement de video

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
-document.getElementById("go").addEventListener("click", getVideo);
+const boutons = Array.from(document.getElementsByClassName("bouton"))
+
+boutons.forEach(bouton => {
+	bouton.addEventListener('click', getVideo)
+})
+
+loadEchauffements();
 
 function getVideo(){
+	const that = this
 	fetch("source.json")
 	.then(function (response) {
 		return response.json();
 	})
 	.then(function (json) {
-		let src = json.src
+		let categorie = that.id
+		let src = json[categorie]
 		let item = src[Math.floor(Math.random()*src.length)]
-		let p_resultat = document.getElementById("resultat")
-		p_resultat.innerHTML = item
+		changeEframe(item)
 	})
+}
+
+function getWarmUp(){
+	changeEframe(this.dataset.src)
+}
+
+function loadEchauffements(){
+	const that = this
+	fetch("source.json")
+	.then(function (response) {
+		return response.json()
+	})
+	.then(function (json) {
+		let src = json.echauffement
+		let barre = document.getElementById("barre-bouton")
+		src.forEach(s => {
+			let b = createBoutonEchaufement(s)
+			barre.appendChild(b)
+		})
+	})
+}
+
+function createBoutonEchaufement(obj){
+	const b = document.createElement("button")
+	const label = document.createTextNode(obj.nom)
+	b.appendChild(label)
+	b.classList.add("echauffement")
+	b.dataset.src = obj.source
+	b.addEventListener('click', getWarmUp)
+	return b
+}
+
+function changeEframe(source){
+	let url = "https://www.youtube.com/embed/" + source
+	document.getElementById("video").src = url
 }
